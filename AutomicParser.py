ddbc1555492f12661a2e3992c9e166cb4e0e9894
DataMadeEasy <info@thomaslrausch.com>
2020-03-02 12:05:13 -0500

    updated code

--- AutomicParser.py
+++ AutomicParser.py
@@ -1,41 +1,81 @@
 import xml.etree.ElementTree as ET
+import os
+
+
+fileFolderStructure = open('M:\\temp\FolderStruct.txt', 'w')
+fileJobs = open('M:\\temp\Jobs.txt', 'w')
 
 
 
 def ParseAutomic(xmlfile):
+    print('**********************************START*************************************')
     
     #load tree into variable
     tree = ET.parse(xmlfile)
 
     root = tree.getroot()
+
+    fileFolderStructure.write('Child|Parent|Type')
+    fileJobs.write('Name|Script')
+
+    for child in root:
+        childElement = child.tag
+        #print('ParseAutomic :' + childElement)
+
+
+        if (childElement == 'FolderStruct'):
+            Parse_FolderStruct(child)
+        elif (childElement== 'JOBS_WINDOWS'):
+            Parse_JOBS_WINDOWS(child)
+        else:
+            print('not found')
+
+
+
     
+
+
+
+
+def Parse_FolderStruct(xmlbranch):
     
-    for item in root:
-        print(item)
+    for item in xmlbranch:
+        print ('In Parse Folder Struct: ' + item.tag )
+        if (item.tag == 'FOLD'):
+            fileFolderStructure.write('\n'+item.attrib['name']+'|root|' + item.tag)
+            print ('In Parse Folder Struct:' + item.tag + item.attrib['name'])
+            Parse_FOLD(item)
+        else:
+            print ('not a folder')
 
 
-    print(root)
+def Parse_FOLD(xmlbranch):
+    print('Parse_Folder')
+
+    for item in xmlbranch:
+        print ('In Parse Folder : ' + item.tag)
+        if (item.tag == 'FOLD'):
+            fileFolderStructure.write('\n'+item.attrib['name']+'|' + xmlbranch.attrib['name'] + '|' + item.tag)
+            Parse_FOLD(item)
+        elif (item.tag == 'JOBS_WINDOWS'):
+            fileFolderStructure.write('\n'+item.attrib['name']+'|' + xmlbranch.attrib['name'] + '|' + item.tag)
+        elif (item.tag == 'JOBS_CIT'):
+            fileFolderStructure.write('\n'+item.attrib['name']+'|' + xmlbranch.attrib['name'] + '|' + item.tag)
+        elif (item.tag == 'JOBP'):
+            fileFolderStructure.write('\n'+item.attrib['name']+'|' + xmlbranch.attrib['name'] + '|' + item.tag)
+        elif (item.tag == 'JOBF_WINDOWS'):
+            fileFolderStructure.write('\n'+item.attrib['name']+'|' + xmlbranch.attrib['name'] + '|' + item.tag)        
+        elif (item.tag == 'EVENT'):
+            fileFolderStructure.write('\n'+item.attrib['name']+'|' + xmlbranch.attrib['name'] + '|' + item.tag)        
+        else:
+            print('parse whats left')
 
 
-
-
-def ParseFolderStruct(xmlbranch):
-    print('ParseFolder')
-
-def ParseFolder(xmlbranch):
-    print('ParseFolder')
-
-def ParseJobsWindows(xmlbranch):
-    print('ParseJobsWindows')
-
-def ParseJobsUnix(xmlbranch):
-    print('ParseJobsWindows')
-
-def ParseJobP(xmlbranch):
-    print('ParseJobsWindows')
-
-
-
+def Parse_JOBS_WINDOWS(xmlbranch):
+    print('JOBS_WINDOWS')
+    for item in xmlbranch:
+        fileJobs.write('\n'+xmlbranch.tag+'|' + item.tag)
+    
 
 
 
