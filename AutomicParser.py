af08c0ffd63803201dcc8f043d43115bba82246d
DataMadeEasy <info@thomaslrausch.com>
2020-03-01 22:09:06 -0500

    First Commit

---
+++ AutomicParser.py
@@ -0,0 +1,49 @@
+import xml.etree.ElementTree as ET
+
+
+
+def ParseAutomic(xmlfile):
+    
+    #load tree into variable
+    tree = ET.parse(xmlfile)
+
+    root = tree.getroot()
+    
+    
+    for item in root:
+        print(item)
+
+
+    print(root)
+
+
+
+def ParseFolderStruct:
+    Print('ParseFolderStruct')
+
+
+def ParseFolderStruct:
+    Print('ParseFolder')
+
+def ParseFolder(xmlbranch):
+    print('ParseFolder')
+
+def ParseJobsWindows(xmlbranch):
+    print('ParseJobsWindows')
+
+def ParseJobsUnix(xmlbranch):
+    print('ParseJobsWindows')
+
+def ParseJobP(xmlbranch):
+    print('ParseJobsWindows')
+
+
+
+
+
+
+
+
+ParseAutomic('Automic_DRM.xml')
+
+
